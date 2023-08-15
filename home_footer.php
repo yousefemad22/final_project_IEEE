f4a0f365d084d57a7e31b8c2414a1a89ad3e71ec
yousef emad <yousef.emad199996@gmail.com>
2023-08-15 23:18:53 +0300

    Create home_footer.php

---
+++ home_footer.php
@@ -0,0 +1,72 @@
+<?php
+
+  include_once("includes/header.php");
+?>
+
+<div class=" p-3 cont mb-0">
+      <div class="container">
+      <div class="row  ">
+        <div class="col-lg-3 d-block py-3">
+          <div class="py-2">
+            <img src="img/restaurant-logo.png" alt="">
+          </div>
+          <div class="py-3">
+            <p style="color:#707070;">Our Restaurant is one of the bests.
+            Provide tasty Menus and Dishes.
+            You can revserve a table or Order food.</p>
+          </div>
+          <div class="icons d-flex ">
+            <i  class="fa-brands fa-facebook px-2"></i>
+            <i  class="fa-brands fa-twitter px-2"></i>
+            <i class="fa-brands fa-instagram px-2"></i>
+            <i class="fa-brands fa-linkedin px-2"></i> 
+            <i class="fa-brands fa-google-plus-g px-2"></i>
+          </div>
+        </div>
+        <div class="col-lg-3 p-4 ff">
+          <p class="f-p">Headquarters</p>
+          <p  style=" color:#707070;">221B Baker Street, P.O Box 353 Park <br>
+              Road, USA - 215431
+            </p>
+           
+        </div>
+
+
+        <div class="col-lg-3 p-4 ff">
+          <p class="f-p">Opening Hours</p>
+          <p  style="font-size:13px;  color:#707070;">Monday-Friday 11:30am-2:00pm</p>
+          <p  style="font-size:13px;  color:#707070;">Monday-Friday 11:30am-2:00pm</p>
+          <p  style="font-size:13px;  color:#707070;">Monday-Friday 11:30am-2:00pm</p>
+          <p  style="font-size:13px;  color:#707070;">Monday-Friday 11:30am-2:00pm</p>
+           
+        </div>
+
+        <div class="col-lg-3 p-4 ff">
+          <p class="f-p">Supscripe to our contents</p>
+          <div class="form-group">
+             <input type="text" class="form-control" style="background-color:#606060; border:none;" placeholder="Enter Address...">
+             <button type="button" class="btn btn-warning my-2 w-100">SUBSCRIBE</button>
+          </div>  
+      </div>
+    <div class="col-lg-12 mt-5 ">
+       <p style="color:#707070;position: absolute;" >&copy;OUR IEEE WEB TEAM</p>
+       <ul style="float:right;">
+        <li class="li">
+            <a href="index.html"> ORDERS</a>
+          </li>
+        <li class="li">
+               <a href="about.html">TERMS</a>
+        </li>
+        <li class="li">
+            <a href="services.html">PROBLEMS</a>
+         </li>
+        
+       </ul> 
+       </div>
+      </div>
+    </div>
+
+      </div>
+<?php
+  include_once("includes/footer.php");
+  ?>
