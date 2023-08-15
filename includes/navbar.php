73fb4f2ea5d17eceea4db1f9e8cd78e307a5f2f8
yousef emad <yousef.emad199996@gmail.com>
2023-08-15 23:19:21 +0300

    Create navbar.php

---
+++ includes/navbar.php
@@ -0,0 +1,44 @@
+<!-- start nav bar in home page -->
+ <!-- start home page -->
+ <?php
+$css_file="home.css";
+?>
+<link rel="stylesheet" href="../css/home.css">
+ <div class="home">
+    <!-- start nav bar in home page -->
+    <div class="navbar navbar-expand-lg fixed-top   mb-5" style="background-color: #212529;">
+      <div class="container">
+        <img src="./img/restaurant-logo.png" class="img1" alt="">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span
+            class="navbar-toggler-icon"></span> </button>
+        <div class="collapse navbar-collapse justify-content-end " id="navbarSupportedContent">
+          <ul class="navbar-nav mb-2 mb-lg-0  ">
+            <li class="nav-item mx-3">
+              <a class=" link3 " href="#" style="">Home</a>
+            </li>
+            <li class="nav-item mx-3">
+              <a class=" link3" href="#menu">Menus</a>
+            </li>
+            <li class="nav-item mx-3">
+              <a class=" link3" href="#gallary">Gallary</a>
+            </li>
+            <li class="nav-item mx-3">
+              <a class=" link3 " href="#">About</a>
+            </li>
+            <li class="nav-item mx-3">
+              <a class=" link3" href="#send_message">Contact</a>
+            </li>
+            <li class="nav-item mx-3">
+              <a class=" active1" href="" style="padding:7px;">Reserve Table</a>
+            </li>
+            <!-- <li class="nav-item mx-3">
+              <a class=" active1" href="" style="padding:7px;">Admin</a>
+            </li> -->
+          </ul>
+        </div>
+
+      </div>
+    </div>
+    <!-- end nav bar in home page -->
+   
